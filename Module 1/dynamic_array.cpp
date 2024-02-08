4eb50109f42080e886dedd8994103c4dc9c69fdf
Jahin Tajowar Masud <148520133+JahinTajowar@users.noreply.github.com>
2024-02-08 20:16:45 +0600

    Add files via upload

---
+++ Module 1/dynamic_array.cpp
@@ -0,0 +1,23 @@
+/*****************
+WAP for Dynamic array.
+****************/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Take an Array size: ";
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    cout<<"Input "<<n<<" Number :";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout <<"Your Taken Number is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout <<a[i] <<" ";
+    }
+    return 0;
+}
